f399ecf6c7c45f524c79596e48b2b24d1db41948
Tharindu37 <tharindut520@gmail.com>
2024-02-04 09:11:45 +0530

    audio record

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import * as RecordRTC from 'recordrtc';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'audio_record';
+  record: any;
+  recording = false;
+  url: any;
+  error: any;
+  constructor(private domSanitizer: DomSanitizer) {}
+
+  sanitize(url: string) {
+    return this.domSanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  startRecording() {
+    this.recording = true;
+    let mediaConstraints = {
+      video: false,
+      audio: true,
+    };
+    navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+
+  successCallback(stream: any) {
+    var options: RecordRTC.Options = {
+      mimeType: 'audio/wav',
+    };
+    var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+    this.record = new StereoAudioRecorder(stream, options);
+    this.record.record();
+  }
+
+  stopRecording() {
+    this.recording = false;
+    this.record.stop(this.processRecording.bind(this));
+  }
+
+  processRecording(blob: any) {
+    this.url = URL.createObjectURL(blob);
+    console.log('blob', blob);
+    console.log('url', this.url);
+  }
+
+  errorCallback(error: any) {
+    this.error = 'Can not play audio in your browser';
+  }
 }
